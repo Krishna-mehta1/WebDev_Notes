8bed420a58afaca4f255bb88e243e32ee21a37ed
Aastha-Bhatia <122430152+Aastha-Bhatia@users.noreply.github.com>
2025-07-15 10:44:42 +0530

    day 7.. started nodejs..

---
+++ day7/node/index.js
@@ -0,0 +1,16 @@
+function subOfTwo(a,b){
+    console.log(a-b);
+    return a/b;
+}
+function divOfTwo(a,b){
+    console.log(a/b);
+    return a/b;
+}
+function sumOfTwo(a,b){
+    console.log(a+b);
+    return a/b;
+}
+
+// module.exports
+
+
